b2e8bfa003d44f832d648eaa3d5b5c1f9cd3282b
James Tomasino <james@tomasino.org>
2022-06-02 14:06:10 +0000

    adds intro text

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="content">
     <h1>
-      Tomasino Glicko2 Film Rankings
+      Tomasino Glicko-2 Film Rankings
     </h1>
     <hr>
+    <p>
+      This site is designed to showcase my watched films list, ranking them from best to worst using the <a href="https://en.wikipedia.org/wiki/Glicko_rating_system">Glicko-2 Rating System</a>. It is an ELO-like system commonly used to rank chess players. This site is an experiment designed to apply that system to my film reviews.
+    </p>
+    <p>
+      To perform these rankings a small number of participants from my wached-films list will be selected for a "ranking tournament". These 10-15 films will be set head-to-head against one another in matches where I will indicate if I prefer one over the other, or whether they tie. Over the course of a tournament each film will compare against each of the others and at the conclusion their rankings will update. Over time these tournaments will sort and reveal my preferences and the rankings will take shape with increasing confidence.
+    </p>
+    <p>
+      The process for this ranking system involves a lot of head-to-head matches before the list reaches a reliable threshhold. As such, it is unlikely to appeal to others for their own personal use. I've decided to keep this site for my personal use only as a result. If you are interested in replicating it, though, all of the code is open source.
+    </p>
     <h2>
       TODO:
     </h2>
@@ -19,7 +28,7 @@
       <ul>
         <li>[ ] Fetch watchlist of user</li>
         <li>[ ] Parse fetched data to fill and append film list into fauna</li>
-        <li>[ ] Estimate provisional Glicko2 ranking based on film stars on letterboxd</li>
+        <li>[ ] Estimate provisional Glicko-2 ranking based on film stars on letterboxd</li>
         <li>[ ] Alternatively, export data from Letterboxd and process it into fauna from CLI</li>
       </ul>
       <li>[ ] Create a public list of all the films and their ranking</li>
@@ -33,9 +42,9 @@
       <ul>
         <li>[ ] Default every film matches against every other in tourney</li>
         <li>[ ] Add option for <a href="https://en.wikipedia.org/wiki/Swiss-system_tournament">Swiss-system tournaments</a></li>
-        <li>[ ] Modify tourney to allow variable spread in Glicko2 pairings</li>
+        <li>[ ] Modify tourney to allow variable spread in Glicko-2 pairings</li>
         <li>[ ] Weigh tourney selection inversely by tourney participation</li>
-        <li>[ ] Update glicko2 ratings after tourney conclusion, not individual match</li>
+        <li>[ ] Update Glicko-2 ratings after tourney conclusion, not individual match</li>
       </ul>
     </ul>
   </div>
@@ -94,5 +103,9 @@ li {
 }
 a {
   color: #f8a6ff;
+}
+p {
+  max-width: 80rem;
+  margin-bottom: 1rem;
 }
 </style>
